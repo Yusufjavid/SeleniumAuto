bba3fe0b3fd185aef76817acedb7c10f2f7aa395
Yusufjavid <33166977+Yusufjavid@users.noreply.github.com>
2025-02-25 18:00:34 -0500

    Test NG changes for beforeMethod and after method test.

--- src/test/java/org/SeleniumAuto1/TestFirstNG.java
+++ src/test/java/org/SeleniumAuto1/TestFirstNG.java
@@ -1,12 +1,36 @@
 package org.SeleniumAuto1;
 
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestFirstNG {
 
-    @Test()
-    void OpenApp(){
-        System.out.println("Open APP");
+    @BeforeMethod
+    void login()
+    {
+        System.out.println("This is login..");
     }
+
+    @AfterMethod
+    void logout()
+    {
+        System.out.println("this is logout...");
+    }
+
+    @Test(priority=1)
+    void search()
+    {
+        System.out.println("this is search...");
+    }
+
+
+    @Test(priority=2)
+    void AdvancedSearch()
+    {
+        System.out.println("this advanced search....");
+    }
+
+
 }
